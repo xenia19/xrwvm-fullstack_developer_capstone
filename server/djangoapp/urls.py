0a4f30f3f36302f1711ae6dd7c22b9130f703bb3
Ksenia <ksiusha.maestra@gmail.com>
2024-08-01 02:31:30 -0400

    Updated

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_user, name='logout'),
     path(route='register', view=views.registration, name='registration'),
-
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for add a review view
 
